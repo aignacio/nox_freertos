473af291adcae8d2301ab8de869fb39bdeb3a5b6
Anderson Ignacio <anderson@aignacio.com>
2022-10-31 18:03:12 +0000

    Updated some functions
    Signed-off-by: Anderson Ignacio <anderson@aignacio.com>

--- app/main.c
+++ app/main.c
@@ -1,177 +1,94 @@
 /* Standard includes. */
-#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+
+/* Kernel includes. */
+#include <FreeRTOS.h>
+#include <task.h>
+#include <timers.h>
+#include <semphr.h>
 
 #include "riscv_csr_encoding.h"
 #include "nox_soc.h"
 #include "uart.h"
 
-#include "FreeRTOS.h" /* Must come first. */
-#include "task.h"     /* RTOS task related API prototypes. */
-#include "queue.h"    /* RTOS queue related API prototypes. */
-#include "timers.h"   /* Software timer related API prototypes. */
-#include "semphr.h"   /* Semaphore related API prototypes. */
+void main( void );
 
-/*-----------------------------------------------------------*/
-uint8_t val;
-
-extern void main_full( void );
-extern void freertos_risc_v_trap_handler( void );
-
-/*
- * Prototypes for the standard FreeRTOS callback/hook functions implemented
- * within this file.  See https://www.freertos.org/a00016.html
- */
-void vApplicationMallocFailedHook( void );
-void vApplicationIdleHook( void );
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
-void vApplicationTickHook( void );
-
-/* Prepare hardware to run the demo. */
-static void prvSetupHardware( void );
-
-/* System */
-void vToggleLED( void );
-void vSendString( const char * pcString );
-
-inline void __attribute__ ((always_inline)) cpu_sleep(void) {
-  //asm volatile ("wfi");
-}
-
-/*-----------------------------------------------------------*/
-
-int main( void )
-{
-	prvSetupHardware();
-
-  uart0_printf("\nFreeRTOS %s on NoX SoC Demo\n\n", tskKERNEL_VERSION_NUMBER);
-  main_full();
-}
-
-/*-----------------------------------------------------------*/
-
-/* Handle specific interrupts */
-void freertos_risc_v_application_interrupt_handler(void) {
-  uint32_t synctrap_cause = 0;
-  // acknowledge/clear ALL pending interrupt sources here - adapt this for your setup
-  write_csr(mip, (0 << IRQ_M_SOFT));
-  write_csr(mip, (0 << IRQ_M_TIMER));
-  write_csr(mip, (0 << IRQ_M_EXT));
-  // debug output - Use the value from the mcause CSR to call interrupt-specific handlers
-  uart0_printf("\n%c\n",getchar());
-  asm volatile("csrr %0,mcause" : "=r"(synctrap_cause));
-  uart0_printf("\n<IRQ> mcause = 0x%x </IRQ>\n", synctrap_cause);
-}
-
-/* Handle specific exceptions */
-void freertos_risc_v_application_exception_handler(void) {
-  uint32_t synctrap_cause = 0;
-  // debug output - Use the value from the mcause CSR to call exception-specific handlers
-  asm volatile("csrr %0,mcause" : "=r"(synctrap_cause));
-  uart0_printf("\n<EXC> mcause = 0x%x </EXC>\n", synctrap_cause);
-}
-
-/*-----------------------------------------------------------*/
-
-static void prvSetupHardware( void )
-{
-  // install the freeRTOS trap handler
-  asm volatile("csrw mtvec, %0":: "r"((uint8_t *)(&freertos_risc_v_trap_handler)));
-
-  // init UART at default baud rate, no parity bits, ho hw flow control
-  uart0_setup(BR_UART);
-}
-
-/*-----------------------------------------------------------*/
-
-void vToggleLED( void )
-{
-  if (val == 1){
-    uart0_print("\nON");
+static void print_task_1(void *pvParameters){
+  uint8_t test = 0;
+  while(1){
+    uart0_printf("\nTask - 1 / Test: %d",test);
+    test += 2;
+    vTaskDelay(pdMS_TO_TICKS(100));
   }
-  else{
-    uart0_print("\nOFF");
+}
+
+static void print_task_2(void *pvParameters){
+  uint8_t test = 0;
+  while(1){
+    uart0_printf("\nTask - 2 / Test: %d",test);
+    test += 4;
+    vTaskDelay(pdMS_TO_TICKS(200));
   }
-
-  val ^= 1;
-	//neorv32_gpio_pin_toggle(0);
 }
 
-/*-----------------------------------------------------------*/
-
-void vSendString( const char * pcString )
-{
-  uart0_print( ( const char * ) pcString );
+static void print_task_3(void *pvParameters){
+  uint8_t test = 0;
+  while(1){
+    uart0_printf("\nTask - 3 / Test: %d",test);
+    test += 10;
+    vTaskDelay(pdMS_TO_TICKS(300));
+  }
 }
 
-/*-----------------------------------------------------------*/
-
-void vApplicationMallocFailedHook( void )
-{
-	/* vApplicationMallocFailedHook() will only be called if
-	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-	function that will get called if a call to pvPortMalloc() fails.
-	pvPortMalloc() is called internally by the kernel whenever a task, queue,
-	timer or semaphore is created.  It is also called by various parts of the
-	demo application.  If heap_1.c or heap_2.c are used, then the size of the
-	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-	to query the size of free heap space that remains (although it does not
-	provide information on how the remaining heap might be fragmented). */
-	taskDISABLE_INTERRUPTS();
-  uart0_print("FreeRTOS_FAULT: vApplicationMallocFailedHook (solution: increase 'configTOTAL_HEAP_SIZE' in FreeRTOSConfig.h)\n");
-	__asm volatile( "ebreak" );
-	for( ;; );
-}
-/*-----------------------------------------------------------*/
-
-void vApplicationIdleHook( void )
-{
-	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-	task.  It is essential that code added to this hook function never attempts
-	to block in any way (for example, call xQueueReceive() with a block time
-	specified, or call vTaskDelay()).  If the application makes use of the
-	vTaskDelete() API function (as this demo application does) then it is also
-	important that vApplicationIdleHook() is permitted to return to its calling
-	function, because it is the responsibility of the idle task to clean up
-	memory allocated by the kernel to any task that has since been deleted. */
-  cpu_sleep();
+static void print_task_4(void *pvParameters){
+  uint8_t test = 0;
+  while(1){
+    uart0_printf("\nTask - 4 / Test: %d",test);
+    test += 10;
+    vTaskDelay(pdMS_TO_TICKS(600));
+  }
 }
 
-/*-----------------------------------------------------------*/
+void main(void){
+  BaseType_t xReturned;
+  TaskHandle_t xHandle1 = NULL,
+               xHandle2 = NULL,
+               xHandle3 = NULL,
+               xHandle4 = NULL;
 
-void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName )
-{
-	( void ) pcTaskName;
-	( void ) pxTask;
+  xReturned = xTaskCreate(
+    print_task_1,
+    "T1",
+    configMINIMAL_STACK_SIZE*2U,
+    NULL,
+    tskIDLE_PRIORITY+1,
+    &xHandle1);
 
-	/* Run time stack overflow checking is performed if
-	configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-	function is called if a stack overflow is detected. */
-	taskDISABLE_INTERRUPTS();
-  uart0_print("FreeRTOS_FAULT: vApplicationStackOverflowHook\n");
-	__asm volatile( "ebreak" );
-	for( ;; );
-}
+  xReturned = xTaskCreate(
+    print_task_2,
+    "I2",
+    configMINIMAL_STACK_SIZE*2U,
+    NULL,
+    tskIDLE_PRIORITY+1,
+    &xHandle2);
 
-/*-----------------------------------------------------------*/
+  xReturned = xTaskCreate(
+    print_task_3,
+    "T3",
+    configMINIMAL_STACK_SIZE*2U,
+    NULL,
+    tskIDLE_PRIORITY+1,
+    &xHandle3);
 
-void vApplicationTickHook( void )
-{
-  /* The tests in the full demo expect some interaction with interrupts. */
-  //extern void vFullDemoTickHook( void );
-  //vFullDemoTickHook();
+  xReturned = xTaskCreate(
+    print_task_4,
+    "T4",
+    configMINIMAL_STACK_SIZE*2U,
+    NULL,
+    tskIDLE_PRIORITY+1,
+    &xHandle4);
 
-  //uart0_print("freeRTOS: vApplicationTickHook");
-  /*uart0_print(".");*/
-}
-
-/*-----------------------------------------------------------*/
-
-/* This handler is responsible for handling all interrupts. Only the machine timer interrupt is handled by the kernel. */
-void SystemIrqHandler( uint32_t mcause )
-{
-  uart0_printf("freeRTOS: Unknown interrupt (0x%x)\n", mcause);
+  vTaskStartScheduler();
+  for(;;);
 }
